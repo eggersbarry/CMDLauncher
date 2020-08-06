930d1f0f87614f6c0d917c9d512cde63a3e588bc
Barry Eggers <eggersbarry@yahoo.com>
2020-08-05 22:28:09 -0400

    Add project files.

---
+++ MainForm1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CmdLauncher
+{
+	partial class MainForm1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtSrcFolder = new System.Windows.Forms.TextBox();
+			this.lbl_src = new System.Windows.Forms.Label();
+			this.txtStartFolder = new System.Windows.Forms.TextBox();
+			this.lbl_start = new System.Windows.Forms.Label();
+			this.lb_files = new System.Windows.Forms.ListBox();
+			this.btnLaunch = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.UD_parameters = new System.Windows.Forms.NumericUpDown();
+			this.lbl_parameters = new System.Windows.Forms.Label();
+			this.lb_filetypes = new System.Windows.Forms.ListBox();
+			((System.ComponentModel.ISupportInitialize)(this.UD_parameters)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtSrcFolder
+			// 
+			this.txtSrcFolder.AllowDrop = true;
+			this.txtSrcFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtSrcFolder.Location = new System.Drawing.Point(138, 12);
+			this.txtSrcFolder.Margin = new System.Windows.Forms.Padding(4);
+			this.txtSrcFolder.Name = "txtSrcFolder";
+			this.txtSrcFolder.Size = new System.Drawing.Size(588, 26);
+			this.txtSrcFolder.TabIndex = 2;
+			this.txtSrcFolder.TextChanged += new System.EventHandler(this.txtSrcFolder_TextChanged);
+			this.txtSrcFolder.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtbox_DragDrop);
+			this.txtSrcFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtbox_DragEnter);
+			this.txtSrcFolder.DragOver += new System.Windows.Forms.DragEventHandler(this.txtbox_DragOver);
+			this.txtSrcFolder.DragLeave += new System.EventHandler(this.txtbox_DragLeave);
+			this.txtSrcFolder.DoubleClick += new System.EventHandler(this.txtSrcFolder_DoubleClick);
+			// 
+			// lbl_src
+			// 
+			this.lbl_src.AutoSize = true;
+			this.lbl_src.Location = new System.Drawing.Point(15, 16);
+			this.lbl_src.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lbl_src.Name = "lbl_src";
+			this.lbl_src.Size = new System.Drawing.Size(111, 18);
+			this.lbl_src.TabIndex = 1;
+			this.lbl_src.Text = "Source Folder";
+			this.lbl_src.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// txtStartFolder
+			// 
+			this.txtStartFolder.AllowDrop = true;
+			this.txtStartFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtStartFolder.Location = new System.Drawing.Point(138, 43);
+			this.txtStartFolder.Margin = new System.Windows.Forms.Padding(4);
+			this.txtStartFolder.Name = "txtStartFolder";
+			this.txtStartFolder.Size = new System.Drawing.Size(588, 26);
+			this.txtStartFolder.TabIndex = 4;
+			this.txtStartFolder.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtbox_DragDrop);
+			this.txtStartFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtbox_DragEnter);
+			this.txtStartFolder.DragOver += new System.Windows.Forms.DragEventHandler(this.txtbox_DragOver);
+			this.txtStartFolder.DragLeave += new System.EventHandler(this.txtbox_DragLeave);
+			// 
+			// lbl_start
+			// 
+			this.lbl_start.AutoSize = true;
+			this.lbl_start.Location = new System.Drawing.Point(15, 47);
+			this.lbl_start.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lbl_start.Name = "lbl_start";
+			this.lbl_start.Size = new System.Drawing.Size(117, 18);
+			this.lbl_start.TabIndex = 3;
+			this.lbl_start.Text = "Start-In Folder";
+			this.lbl_start.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lb_files
+			// 
+			this.lb_files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lb_files.FormattingEnabled = true;
+			this.lb_files.HorizontalScrollbar = true;
+			this.lb_files.ItemHeight = 18;
+			this.lb_files.Location = new System.Drawing.Point(18, 93);
+			this.lb_files.Name = "lb_files";
+			this.lb_files.Size = new System.Drawing.Size(344, 472);
+			this.lb_files.TabIndex = 5;
+			// 
+			// btnLaunch
+			// 
+			this.btnLaunch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnLaunch.Location = new System.Drawing.Point(15, 584);
+			this.btnLaunch.Name = "btnLaunch";
+			this.btnLaunch.Size = new System.Drawing.Size(84, 26);
+			this.btnLaunch.TabIndex = 9;
+			this.btnLaunch.Text = "&Launch";
+			this.btnLaunch.UseVisualStyleBackColor = true;
+			this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExit.Location = new System.Drawing.Point(636, 584);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(84, 26);
+			this.btnExit.TabIndex = 10;
+			this.btnExit.Text = "E&xit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// UD_parameters
+			// 
+			this.UD_parameters.Location = new System.Drawing.Point(374, 115);
+			this.UD_parameters.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.UD_parameters.Name = "UD_parameters";
+			this.UD_parameters.Size = new System.Drawing.Size(86, 26);
+			this.UD_parameters.TabIndex = 7;
+			this.UD_parameters.ValueChanged += new System.EventHandler(this.UD_parameters_ValueChanged);
+			// 
+			// lbl_parameters
+			// 
+			this.lbl_parameters.AutoSize = true;
+			this.lbl_parameters.Location = new System.Drawing.Point(371, 93);
+			this.lbl_parameters.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lbl_parameters.Name = "lbl_parameters";
+			this.lbl_parameters.Size = new System.Drawing.Size(94, 18);
+			this.lbl_parameters.TabIndex = 6;
+			this.lbl_parameters.Text = "Parameters";
+			this.lbl_parameters.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lb_filetypes
+			// 
+			this.lb_filetypes.FormattingEnabled = true;
+			this.lb_filetypes.ItemHeight = 18;
+			this.lb_filetypes.Location = new System.Drawing.Point(475, 82);
+			this.lb_filetypes.Name = "lb_filetypes";
+			this.lb_filetypes.Size = new System.Drawing.Size(134, 58);
+			this.lb_filetypes.TabIndex = 8;
+			this.lb_filetypes.SelectedIndexChanged += new System.EventHandler(this.lb_filetypes_SelectedIndexChanged);
+			// 
+			// MainForm1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ClientSize = new System.Drawing.Size(734, 623);
+			this.Controls.Add(this.lb_filetypes);
+			this.Controls.Add(this.lbl_parameters);
+			this.Controls.Add(this.UD_parameters);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnLaunch);
+			this.Controls.Add(this.lb_files);
+			this.Controls.Add(this.lbl_start);
+			this.Controls.Add(this.txtStartFolder);
+			this.Controls.Add(this.lbl_src);
+			this.Controls.Add(this.txtSrcFolder);
+			this.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm1";
+			this.Text = "CmdLauncher";
+			this.Load += new System.EventHandler(this.MainForm1_Load);
+			this.Resize += new System.EventHandler(this.MainForm1_Resize);
+			((System.ComponentModel.ISupportInitialize)(this.UD_parameters)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtSrcFolder;
+		private System.Windows.Forms.Label lbl_src;
+		private System.Windows.Forms.TextBox txtStartFolder;
+		private System.Windows.Forms.Label lbl_start;
+		private System.Windows.Forms.ListBox lb_files;
+		private System.Windows.Forms.Button btnLaunch;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.NumericUpDown UD_parameters;
+		private System.Windows.Forms.Label lbl_parameters;
+		private System.Windows.Forms.ListBox lb_filetypes;
+	}
+}
+
---
+++ MainForm1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.IO;
+//using System.Data;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CmdLauncher
+{
+	public enum FILETYPES
+	{
+		CMD = 0,
+		BAT = 1,
+		PS = 2
+	}
+	public partial class MainForm1 : Form
+	{
+		private string[] _thesefiles = { "*.cmd", "*.bat", "*.ps" };
+		private int _maxparams = 10;
+		private string thistxtboxname = "tbparam";
+		private int borderv;
+		private int borderh;
+
+
+		public MainForm1()
+		{
+			InitializeComponent();
+		}
+
+		#region system
+		private void MainForm1_Load(object sender, EventArgs e)
+		{
+			borderv = this.Margin.Vertical;
+			borderh = this.Margin.Horizontal;
+			lb_filetypes.Items.Clear();
+			foreach (string ft in _thesefiles)
+			{
+				lb_filetypes.Items.Add(ft);
+			}
+			lb_filetypes.SelectedIndex = 0;
+		}
+
+		private void MainForm1_Resize(object sender, EventArgs e)
+		{
+			int borderv = this.Margin.Vertical;
+			int borderh = this.Margin.Horizontal;
+			System.Windows.Forms.TextBox[] theseboxes;
+			lbl_parameters.Top = lb_files.Top;
+			lbl_parameters.Left = lb_files.Left + lb_files.Width + borderh;
+			UD_parameters.Top = lb_files.Top + lbl_parameters.Height + borderv;
+
+			UD_parameters.Left = lbl_parameters.Left;
+			lb_filetypes.Left = UD_parameters.Left + UD_parameters.Width + borderh;
+			lb_filetypes.Top = lbl_parameters.Top;
+			theseboxes = GetParameters().ToArray();
+			if (theseboxes != null)
+			{
+				foreach (System.Windows.Forms.TextBox tb in theseboxes)
+				{
+					tb.Left = UD_parameters.Left;
+					tb.Width = (this.ClientRectangle.Width) - (lb_files.Left + lb_files.Width + borderh) - borderh;
+				}
+			}
+
+		}
+		private void lb_filetypes_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			FillFileList(txtSrcFolder, lb_files);
+		}
+		private void txtbox_DragDrop(object sender, DragEventArgs ee)
+		{
+			StringBuilder sb = new StringBuilder();
+			String[] files;
+			System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+
+
+			files = (String[])ee.Data.GetData(DataFormats.FileDrop);
+			foreach (string ss in files) // should be only one
+			{
+				if (ss.Length > 0)
+				{
+					sb.Append(ss);
+					break;
+				}
+			}
+			((TextBox)sender).Text = sb.ToString().Replace("\r\n", "");
+		}
+
+		private void txtbox_DragEnter(object sender, DragEventArgs ee)
+		{
+			if (ee.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				ee.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				ee.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void txtbox_DragLeave(object sender, EventArgs e)
+		{
+			// something
+		}
+
+		private void txtbox_DragOver(object sender, DragEventArgs e)
+		{
+			//something
+		}
+		#endregion
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void txtSrcFolder_DoubleClick(object sender, EventArgs e)
+		{
+			if (SelectFolder(sender, e))
+			{
+				//FillFileList(txtSrcFolder, listView1);
+				FillFileList(txtSrcFolder, lb_files);
+			}
+			else
+			{
+				lb_files.Items.Clear();
+			}
+		}
+
+		private bool SelectFolder(object sender, EventArgs e)
+		{
+			bool retval = false;
+			System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+			System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+			if (System.IO.Directory.Exists(tb.Text))
+			{
+				fbd.SelectedPath = tb.Text;
+			}
+			System.Windows.Forms.DialogResult dr = fbd.ShowDialog();
+			if (dr == System.Windows.Forms.DialogResult.OK)
+			{
+				tb.Text = fbd.SelectedPath;
+				retval = true;
+			}
+			else
+			{
+				tb.Text = "";
+			}
+			return retval;
+		}
+
+		private bool FillFileList(TextBox whichbox, ListBox view1)
+		{
+			DirectoryInfo di;
+			bool retval = false;
+			int ft = lb_filetypes.SelectedIndex;
+			if (Directory.Exists(whichbox.Text))
+			{
+				di = new DirectoryInfo(whichbox.Text);
+				FileInfo[] files = di.GetFiles(_thesefiles[ft]);
+				view1.Items.Clear();
+				foreach (FileInfo file in files)
+				{
+					view1.Items.Add(file.Name);
+				}
+				retval = true;
+			}
+			return retval;
+		}
+		private void txtSrcFolder_TextChanged(object sender, EventArgs e)
+		{
+			if (!FillFileList(txtSrcFolder, lb_files))
+			{
+				lb_files.Items.Clear();
+			}
+		}
+
+		private System.Collections.Generic.List<System.Windows.Forms.TextBox> GetParameters()
+		{
+			System.Collections.Generic.List<System.Windows.Forms.TextBox> theseboxes = new System.Collections.Generic.List<System.Windows.Forms.TextBox>();
+			string txtname;
+
+			for (int ii = 0; ii < _maxparams; ii++)
+			{
+				txtname = thistxtboxname + ii.ToString();
+				if (this.Controls.ContainsKey(txtname))
+				{
+					theseboxes.Add((System.Windows.Forms.TextBox)this.Controls[txtname]);
+				}
+			}
+			return theseboxes;
+		}
+
+		private void UD_parameters_ValueChanged(object sender, EventArgs e)
+		{
+			System.Windows.Forms.TextBox tb;
+			System.Collections.Generic.List<System.Windows.Forms.TextBox> theseboxes;
+			int thisvalue = (int)UD_parameters.Value;
+			System.Drawing.Rectangle rect;
+			rect = UD_parameters.Bounds;
+			rect.Y += (rect.Height + borderv);
+			rect.Width = this.ClientRectangle.Width - rect.X - borderh;
+			for (int ii = 0; ii < thisvalue; ii++)
+			{
+				tb = new System.Windows.Forms.TextBox();
+				tb.Name = thistxtboxname + ii.ToString();
+				//tb.Text = tb.Name;
+				tb.Anchor = AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Top;
+				tb.Bounds = rect;
+				rect.Y += (rect.Height + borderv);
+				if (!this.Controls.ContainsKey(tb.Name))
+				{
+					this.Controls.Add(tb);
+				}
+				else
+				{
+					tb.Dispose();
+					tb = null;
+				}
+			}
+			theseboxes = GetParameters();
+			if (theseboxes != null)
+			{
+				for (int ii = 9; ii > thisvalue - 1; ii--)
+				{
+					foreach (System.Windows.Forms.TextBox tb1 in theseboxes)
+					{
+						if (tb1.Name.Equals(thistxtboxname + ii.ToString()))
+						{
+							this.Controls.Remove(tb1);
+							tb1.Dispose();
+							//tb1 = null;
+							break;
+						}
+					}
+				}
+
+			}
+		}
+
+		private void btnLaunch_Click(object sender, EventArgs e)
+		{
+			string filename;
+			string parameters = System.String.Empty;
+			System.Collections.Generic.List<System.Windows.Forms.TextBox> theseboxes;
+			System.Diagnostics.Process newproc;
+			if (lb_files.SelectedIndex >= 0)
+			{
+				filename = (txtSrcFolder.Text.EndsWith("\\") ? txtSrcFolder.Text : txtSrcFolder.Text + "\\") + lb_files.SelectedItem.ToString();
+				if (!System.String.IsNullOrEmpty(filename))
+				{
+					if (System.IO.File.Exists(filename))
+					{
+						theseboxes = GetParameters();
+						foreach (System.Windows.Forms.TextBox tb in theseboxes)
+						{
+							//parameters += " \"" + tb.Text + "\"";
+							parameters += " " + tb.Text;
+						}
+						newproc = new System.Diagnostics.Process();
+						newproc.StartInfo.FileName = filename;
+						newproc.StartInfo.Arguments = parameters;
+						newproc.StartInfo.WorkingDirectory = txtStartFolder.Text;
+						newproc.Start();
+					}
+
+				}
+			}
+		}
+
+	}
+}
